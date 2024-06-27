66e55c34300c034b6c50051fb38f9880b2e95b49
Harshad Kachhadiya <harshad.r.k.100@gmail.com>
2024-06-27 09:53:22 +0530

    update approve address

--- frontend/src/Modal/BuyBondsBru.js
+++ frontend/src/Modal/BuyBondsBru.js
@@ -86,7 +86,7 @@ const BuyBondsBru = ({ open, handleClose }) => {
             let data = await getTokenAllowance(
                 address,
                 selectedOption.id == 1 ? "USDT" : "USDC",
-                environment.contracts[chainId].RouterAddress,
+                environment.contracts[chainId].PoolAddress,
                 signer,
                 chainId,
             );
